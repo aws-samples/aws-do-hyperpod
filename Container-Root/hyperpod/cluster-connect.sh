3868480ee116bcc27b60e8cce4cb606e0bde2955
Alex Iankoulski <iankouls@amazon.com>
2024-09-12 20:38:52 -0400

    Add cluster-connect.sh

---
+++ Container-Root/hyperpod/cluster-connect.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ./conf/env.conf
+source ${CONF}/env_input
+
+usage(){
+	echo ""
+	echo "Usage: $0 <cluster_name>
+	echo ""
+}
+
+if [ "$1" == "" ]; then
+	help
+else
+	echo ""
+	echo "Connecting to $IMPL cluster: $1 "
+
+	impl/${IMPL}/cluster-connect.sh $1
+fi
---
+++ Container-Root/hyperpod/impl/eks/cluster-connect.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+help(){
+	echo ""
+	echo "Usage: $0 <cluster_name>
+	echo ""
+}
+
+if [ "$1" == "" ]; then
+	help
+else
+	CMD="aws eks update-kubeconfig --name ${1} ${ENDPOINT_ARG}"
+	if [ ! "$VERBOSE" == "false" ]; then echo -e "\n${CMD}\n"; fi
+	eval "$CMD"
+fi
+
