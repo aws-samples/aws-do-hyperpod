f27f4cd64f61da89f3c7bd9159bae8d59d266e3d
Alex Iankoulski <iankouls@amazon.com>
2024-10-16 16:49:56 -0700

    add deep health check status to watch nodes command

--- Container-Root/hyperpod/ops/eks/watch-nodes.sh
+++ Container-Root/hyperpod/ops/eks/watch-nodes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watch kubectl get nodes -L node.kubernetes.io/instance-type -L sagemaker.amazonaws.com/node-health-status "$@"
+watch kubectl get nodes -L node.kubernetes.io/instance-type -L sagemaker.amazonaws.com/node-health-status -L sagemaker.amazonaws.com/deep-health-check-status "$@"
